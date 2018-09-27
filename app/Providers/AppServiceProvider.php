3199f21e872a5b70bad10c66db997cdf2689e9d2
freecloud0 <freecloud.sac@gmail.com>
2018-09-26 21:22:38 -0500

    img

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,16 +6,22 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    
+    public function boot(UrlGenerator $url)
+    {
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+    }
+ 
     /**
      * Register any application services.
      *
      * @return void
      */
-    
+    public function register()
+    {
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+    }
 }
